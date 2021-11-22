12ca0dbde1b3968ea456f2ebb772f4159becbf32
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2021-11-22 12:52:51 +0400

    Added simple filter scripts.

---
+++ #src/scripts/_filter.js
@@ -0,0 +1,21 @@
+function filtContentByType(e) {
+  let typeOfContent = e.target.dataset.filtContent;
+
+  for (const work of works) {
+    if (typeOfContent === 'all' || work.dataset.contentType === typeOfContent) {
+      work.style.display = '';
+    } else {
+      work.style.display = 'none';
+    }
+  }
+}
+const filterButtons = document.querySelectorAll('[data-filt-content]');
+for (const filtButton of filterButtons) {
+  filtButton.addEventListener('click', filtContentByType);
+}
+const works = document.querySelectorAll('[data-content-type]');
+
+/*
+? For working add data-attributes data-filt-content="type" for filter buttons,
+? And data-content-type="type" for content-blocks.
+*/
