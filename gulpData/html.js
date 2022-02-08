3710e79deba787eec9e84511f75f22347e798c91
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2022-02-08 13:17:22 +0400

    Build optimization. Build is now executed in two modes: normal and prod. The prod mode includes: Image conversion and optimization. Automatic connection of images in new formats. Script compression.
    P.S: Switching to es2018.

--- gulpData/html.js
+++ gulpData/html.js
@@ -4,7 +4,11 @@ import webpAvifHTML from 'gulp-avif-webp-html';
 export function html() {
   return gulp.src(paths.scr.html)
     .pipe(fileInclude())
-    .pipe(webpAvifHTML())
+    .pipe(
+      global.if(global.isProd,
+        (webpAvifHTML())
+      )
+    )
     .pipe(gulp.dest(paths.build.html))
     .pipe(browsersync.stream());
 }
