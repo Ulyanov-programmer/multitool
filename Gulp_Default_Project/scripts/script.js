69474a6157b6714764d6a9ca7e4230ff06b9c2ab
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2022-03-07 12:35:44 +0400

    Fix demo page.

--- Gulp_Default_Project/scripts/script.js
+++ Gulp_Default_Project/scripts/script.js
@@ -14,46 +14,43 @@ SpoilerMenu.contentActiveClass = 'active';
 // filter //
 import Filter from './modules/filter.js';
 /*
-  For working add data-attributes [ data-filt-content="type" ] for filter buttons,
-  And [ data-content-type="type" ] for content-blocks.
+    When you press the filter__button, all elements that do not have
+    a value similar to the filter__button attribute value will be hidden.
+    To reset the filter through a filter__button,
+    you should use the 'all' value for the data-filt-content attribute.
 
-  When you press the filter__button, all elements that do not have
-  a value similar to the filter__button attribute value will be hidden.
-  To reset the filter through a filter__button,
-  you should use the 'all' value for the filter__button attribute.
-
-  You can declare multiple filters to work separately.
+    You can declare multiple filters to work separately.
 */
 let filter = new Filter('.filter__button', '.filter__item');
 // modal window //
 import ModalWindowMenu from './modules/modalWindow.js';
 /*
-  It works like this:
-  Finds all elements that contain data-modal-link and .modal-closer.
-  The first ones trigger the event of opening the modal window, the second ones close it.
-  After clicking on one of these elements,
-  it looks for a block with a name inside data-modal-link and gives it the "active" class.
+    It works like this:
+    Finds all elements that contain data-modal-link and .modal-closer.
+    The first ones trigger the event of opening the modal window, the second ones close it.
+    After clicking on one of these elements,
+    it looks for a block with a name inside data-modal-link and gives it the "active" class.
 
-  Also, when pressed, turns off the scrolling page.
+    Also, when pressed, turns off the scrolling page.
 */
 new ModalWindowMenu('[data-modal-link]', // i recommend this value.
 '.modal-closer', 500, '.fullscreen-navmenu');
 // element-modal //
 import ElementModal from './modules/elementMenu.js';
 /*
-  It works like this:
-  When hovering over a contentElement,
-  clones the modalElement and places it absolutely above the contentElement.
-  Moving the mouse away from the contentElement deletes the pasted modalElement copy.
+    It works like this:
+    When hovering over a contentElement,
+    clones the modalElement and places it absolutely above the contentElement.
+    Moving the mouse away from the contentElement deletes the pasted modalElement copy.
 */
 let elementMenu1 = new ElementModal('.el-menu__item', '.el-menu__menu', 300);
 // scroll-elements //
 import ScrollElement from './modules/scrollToElement.js';
 /*
-  Use if you want to use buttons for scrolling.
-  How it works?
-  When you press a scrollButton, you scroll to the block
-  indicated in data-scroll-to attribute of this scrollButton.
+    Use if you want to use buttons for scrolling.
+    How it works?
+    When you press a scrollButton, you scroll to the block
+    indicated in data-scroll-to attribute of this scrollButton.
 */
 let scrollElement = new ScrollElement('[data-scroll-to]', // i recommend use this value.
 '.fixed-header');
@@ -65,9 +62,9 @@ SidebarMenu.buttonsActiveClass = 'active';
 // accordion //
 import Accordion from './modules/accord.js';
 /* Attention,
-  the display property for inactive accordion elements
-  does not allow them to be hidden.
-  If you need to use it, set it via the active element class.
+    the display property for inactive accordion elements
+    does not allow them to be hidden.
+    If you need to use it, set it via the active element class.
 */
 let someAccrod = new Accordion('.accordion__btn', '.accordion__item', 500);
 someAccrod.buttonsActiveClass = 'active';
