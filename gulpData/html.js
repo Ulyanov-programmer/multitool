6064eaf21fa7d79098a58631e485f04dbaa68111
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2022-05-17 13:52:40 +0400

    test changes, trying to use 'gulp-html-img-to-picture'

--- gulpData/html.js
+++ gulpData/html.js
@@ -1,12 +1,27 @@
 import fileInclude from 'gulp-file-include';
-import webpAvifHTML from 'gulp-avif-webp-html';
+import plumber from "gulp-plumber"
+import imgToPicture from "gulp-html-img-to-picture"
 
 export function html() {
 	return gulp.src(paths.scr.html)
 		.pipe(fileInclude())
+		.pipe(plumber({
+			errorHandler: function (err) {
+				notify.onError({
+					title: "HTML img to picture Error",
+					message: "<%= error.message %>"
+				})(err)
+			}
+		}))
 		.pipe(
 			global.if(global.isProd,
-				(webpAvifHTML())
+				(imgToPicture({
+					sortBySize: false,
+					sourceExtensions: [
+						{ extension: 'webp', mimetype: 'image/webp', },
+						{ extension: 'avif', mimetype: 'image/avif', },
+					],
+				}))
 			)
 		)
 		.pipe(gulp.dest(paths.build.html))
