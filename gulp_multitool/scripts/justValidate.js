c99990fdf51ca94856dfbf2e1516f999028de2a6
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2022-07-28 13:20:16 +0400

    Additions in JustValidate scripts.
    Scripts have been added thanks to which form submission is possible when using the standard form action handler.
    
    Now the error message is positioned above the input by default.

--- gulp_multitool/scripts/justValidate.js
+++ gulp_multitool/scripts/justValidate.js
@@ -1,11 +1,17 @@
 const someValidation = new JustValidate("#form", {
-  errorFieldCssClass: "invalid",
-  errorLabelCssClass: "invalid",
-  errorLabelStyle: {
-    fontSize: "14px",
-    color: "white"
-  },
-  errorsContainer: "#errors_container"
-});
+	errorFieldCssClass: "invalid",
+	errorLabelCssClass: "invalid",
+	errorLabelStyle: {
+		position: 'absolute',
+		bottom: '110%',
+		left: '0px',
+		fontSize: "14px",
+		color: "white"
+	},
+	errorsContainer: "#errors_container"
+})
 someValidation.onSuccess((e) => {
-});
+	ev.preventDefault()
+	window.showNotification()
+	ev.target.submit()
+})
