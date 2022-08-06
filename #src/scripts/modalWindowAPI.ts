c216c0d3b20625cba44b8581ca6653c8b38a2450
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2022-08-06 18:26:45 +0400

    Changes in the modal window module.
    * Code optimization.
    * Removed the delay after closing the window.

--- #src/scripts/modalWindowAPI.ts
+++ #src/scripts/modalWindowAPI.ts
@@ -10,7 +10,6 @@ import ModalWindowMenu from './modules/modalWindow.js'
 */
 new ModalWindowMenu({
 	modalLinksSelector: '[data-open-modal-id]',
-	modalClosersSelector: '[data-modal-close]',
+	modalClosersSelector: '[data-modal-closer]',
 	burgerMenuSelector: '.burger-menu',
-	// smooth transition is calculated automatically, specify it in  modal styles.
 })
--- #src/scripts/modules/modalWindow.ts
+++ #src/scripts/modules/modalWindow.ts
@@ -18,10 +18,12 @@ interface ModalWindowMenuArgs {
 
 export default class ModalWindowMenu {
 	private static modalLinks: NodeListOf<HTMLElement>
+	private static modalElements: NodeListOf<HTMLElement>
 	private static modalClosers: NodeListOf<HTMLElement>
 	private static burgerMenuClasslist: DOMTokenList
-	private static UNLOCK: boolean = true
-	public static transitionTimeout: number
+	private readonly modalWindowSelector = '.modal-window'
+	private readonly modalWindowContentClass = 'modal-window__body'
+	private readonly modalWindowActiveSelector = '.modal-window.active'
 
 	constructor(arg: ModalWindowMenuArgs) {
 		if (!elementIsExistWithLog('ModalWindowMenu',
@@ -41,19 +43,31 @@ export default class ModalWindowMenu {
 				if (modalId) {
 					let modal = document.getElementById(modalId)
 
-					ModalWindowMenu.transitionTimeout = parseFloat(getComputedStyle(modal)
-						.getPropertyValue('transition-duration')) * 1000
-
 					this.showOrHideModal(modal)
 				}
 			})
 		}
 
+
+		ModalWindowMenu.modalElements = document.querySelectorAll(this.modalWindowSelector)
+
+		for (let modal of ModalWindowMenu.modalElements) {
+			modal.addEventListener("click", (e) => {
+				let target = e.target as Element
+
+				// Checks if there was a click on the empty space around. If true, hides a window.
+				if (target.classList.contains(this.modalWindowContentClass)) {
+					this.closeActiveModal(true)
+				}
+			})
+		}
+
+
 		ModalWindowMenu.modalClosers = document.querySelectorAll(arg.modalClosersSelector)
 
 		for (let modalCloser of ModalWindowMenu.modalClosers) {
 			modalCloser.addEventListener("click", () =>
-				this.closeModal(modalCloser.closest('.modal-window'), true)
+				this.closeActiveModal()
 			)
 		}
 
@@ -61,38 +75,27 @@ export default class ModalWindowMenu {
 		document.addEventListener('keydown', (key) => {
 			if (key.code != 'Escape') return
 
-			let activeModal = document.querySelector<HTMLElement>('.modal-window.active')
-			activeModal ? this.closeModal(activeModal, true) : false
+			let activeModal = this.getCurrentActiveModal()
+			activeModal ? this.closeActiveModal() : false
 		})
 	}
 
 
-	private showOrHideModal(modalElement) {
-		if (modalElement && ModalWindowMenu.UNLOCK) {
-			let activeModal = document.querySelector<HTMLElement>('.modal-window.active')
+	private showOrHideModal(modalElement: HTMLElement) {
+		let activeModal = this.getCurrentActiveModal()
 
-			activeModal ? this.closeModal(activeModal, false) : this.toggleBodyScroll(false)
+		activeModal ? this.closeActiveModal(false, activeModal) : this.toggleBodyScroll(false)
 
-			modalElement.classList.add("active")
-		}
-		modalElement.addEventListener("click", (e) => {
-
-			// Checks if the pressed element has a CONTENT parent, if not, closes the modal.
-			if (e.target.closest('.modal-window__content') == null) {
-				this.closeModal(modalElement, true)
-			}
-		})
+		modalElement.classList.add("active")
 	}
 
-	private closeModal(modalWindow: HTMLElement, bodyIsScrollable: boolean) {
-		if (ModalWindowMenu.UNLOCK == false) return
+	private closeActiveModal(bodyIsScrollable: boolean = true, activeModal?: HTMLElement) {
+		if (activeModal == undefined)
+			activeModal = this.getCurrentActiveModal()
 
-		modalWindow.classList.remove("active")
+		activeModal.classList.remove("active")
 
-		setTimeout(() => {
-			bodyIsScrollable ? this.toggleBodyScroll(true) : false
-
-		}, ModalWindowMenu.transitionTimeout * 2)
+		bodyIsScrollable ? this.toggleBodyScroll(true) : false
 	}
 
 	private toggleBodyScroll(toggleScrollOn: boolean) {
@@ -103,17 +106,15 @@ export default class ModalWindowMenu {
 			document.body.style.paddingRight = returnScrollbarWidth() + 'px'
 			document.body.style.overflow = 'hidden'
 		}
+	}
 
-		ModalWindowMenu.UNLOCK = false
-
-		// Prevents a new window from opening too quickly.
-		setTimeout(() => {
-			ModalWindowMenu.UNLOCK = true
-		}, ModalWindowMenu.transitionTimeout * 2)
+	private getCurrentActiveModal(): HTMLElement {
+		let activeModal = document.querySelector(this.modalWindowActiveSelector) as HTMLElement
+		return activeModal
 	}
 
 
-	private chekPossibileSwitchScroll(toggleOnValue: boolean) {
+	private chekPossibileSwitchScroll(toggleOnValue: boolean): boolean {
 		if (ModalWindowMenu.burgerMenuClasslist) {
 			if (!ModalWindowMenu.burgerMenuClasslist.contains('active') && toggleOnValue) {
 				return true
