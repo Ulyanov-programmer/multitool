fec8aa2a8918a2b53ed29afb8e1e0c78117e41d5
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2023-01-30 12:23:59 +0400

    [formatting] Replacing double quotes with single quotes.

--- src/php/mail.php
+++ src/php/mail.php
@@ -7,13 +7,13 @@ require 'Exception.php';
 
 $c     = true;
 // $file = $_FILES['file'];
-$title = "Title";
+$title = 'Title';
 
 // Tabular data layout.
 foreach ($_POST as $key => $value) {
-  if ($value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject") {
-    $body .= "
-    " . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+  if ($value != '' && $key != 'project_name' && $key != 'admin_email' && $key != 'form_subject') {
+    $body .= '
+    ' . (($c = !$c) ? '<tr>' : "<tr style='background-color: #f8f8f8;'>") . "
       <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
       <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
     </tr>
@@ -27,7 +27,7 @@ $mail = new PHPMailer\PHPMailer\PHPMailer();
 
 try {
   $mail->isSMTP();
-  $mail->CharSet    = "UTF-8";
+  $mail->CharSet    = 'UTF-8';
   $mail->SMTPAuth   = true;
 
   // Settings of PHPMailer
@@ -49,9 +49,9 @@ try {
   //     $filename = $file['name'][$ct];
   //     if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
   //         $mail->addAttachment($uploadfile, $filename);
-  //         $rfile[] = "Successfully! $filename";
+  //         $rfile[] = 'Successfully! $filename';
   //     } else {
-  //         $rfile[] = "Error! $filename";
+  //         $rfile[] = 'Error! $filename';
   //     }
   //   }
   // }
