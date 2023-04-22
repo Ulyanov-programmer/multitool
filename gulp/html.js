cac3b285bfe7670ad8e5daf41fd1d32079f50595
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2023-04-22 09:05:52 +0400

    [html] Implemented PostHTML, transition EJS => posthtml-components.
    Now HTML is processed by the PostHTML postprocessor.
    Implemented plugins: posthtml-easy-bem, posthtml-beautify, posthtml-component.
    
    Redesigned html to work with posthtml-component instead of ejs.

--- gulp/html.js
+++ gulp/html.js
@@ -1,23 +1,26 @@
 import gulp from 'gulp'
 import browsersync from 'browser-sync'
-import ejs from 'gulp-ejs'
 import imgToPicture from 'gulp-html-img-to-picture'
 import versionNumber from 'gulp-version-number'
 import posthtml from 'gulp-posthtml'
 import easyBem from 'posthtml-easy-bem'
 import component from 'posthtml-component'
+import beautify from 'posthtml-beautify'
 import gulpIf from 'gulp-if'
 import { paths } from './paths.js'
 const isProd = process.argv.includes('--prod')
 
 export default function html() {
   return gulp.src(paths.scr.html)
-    .pipe(ejs({}))
     .pipe(posthtml([
       easyBem(),
       component({
         root: './src',
         folders: ['components'],
+      }),
+      beautify({
+        rules: {
+        }
       }),
     ], {}))
 
