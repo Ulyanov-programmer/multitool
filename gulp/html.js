40064b823a09bdc262d9fb63dc9dbfd245b0b01c
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2023-05-14 19:06:20 +0400

    [npm] Migration to yarn, step 3.
    Changes have been made for PlugAndPlay to work.
    
    Now gulp starting with the "yarn dev" command.

--- gulp/html.js
+++ gulp/html.js
@@ -4,8 +4,9 @@ import imgToPicture from 'gulp-html-img-to-picture'
 import versionNumber from 'gulp-version-number'
 import posthtml from 'gulp-posthtml'
 import easyBem from 'posthtml-easy-bem'
-import component from 'posthtml-component'
-import beautify from 'posthtml-beautify'
+// TODO: Enable this for finding an error.
+// import component from 'posthtml-component'
+// import beautify from 'posthtml-beautify'
 import imgAutosize from 'posthtml-img-autosize'
 import gulpIf from 'gulp-if'
 import { htmlValidator } from 'gulp-w3c-html-validator'
@@ -16,16 +17,16 @@ export default function html() {
   return gulp.src(paths.scr.html)
     .pipe(posthtml([
       easyBem(),
-      component({
-        root: './src',
-        folders: ['components'],
-      }),
-      beautify({
-        rules: {
-          blankLines: false,
-          sortAttr: true,
-        }
-      }),
+      // component({
+      //   root: './src',
+      //   folders: ['components'],
+      // }),
+      // beautify({
+      //   rules: {
+      //     blankLines: false,
+      //     sortAttr: true,
+      //   }
+      // }),
       imgAutosize({
         processEmptySize: true,
       })
