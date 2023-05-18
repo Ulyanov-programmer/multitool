ceba5f0374e36ab0f8398060e9845108cd1c4514
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2023-05-18 16:12:32 +0400

    [fonts] Improved the task for fonts.
    Added the ability to configure the rules for connecting fonts at run.

--- gulp/fonts.js
+++ gulp/fonts.js
@@ -1,9 +1,10 @@
-import fs from 'fs-extra'
-import ttf2woff2 from 'gulp-ttf2woff2'
 import gulp from 'gulp'
-import { fontsFIlePath } from './paths.js'
-import { paths } from './paths.js'
+import ttf2woff2 from 'gulp-ttf2woff2'
+import fs from 'fs-extra'
+import enquirer from 'enquirer'
+import chalk from 'chalk'
 import { parseNumericWeightFromName, parseStyleFromName } from 'parse-font-name'
+import { fontsFIlePath, paths } from './paths.js'
 
 export default function fonts() {
   gulp.src(paths.scr.fontsWoff)
@@ -22,49 +23,72 @@ export function fontsStyle() {
   if (fontsFileContent.length > 0)
     return
 
-  fs.readdir(paths.scr.fontsFolder, (err, fileNames) => {
-    let previousFontName
-
+  fs.readdir(paths.scr.fontsFolder, async (err, fileNames) => {
     if (fileNames == undefined) {
-      console.log('No font was found!')
+      console.log('No one font was found!')
       return
     }
+
+    let previousFontName
+    // removing the .gitkeep from fileNames.
+    fileNames.splice(fileNames.indexOf('.gitkeep'), 1)
+
+    if (fileNames.length == 0) {
+      console.log('No one font was found!')
+      return
+    } else {
+      console.log(
+        chalk.green.bold("Hey, i see that you have fonts, but i haven't connected them yet. Let me help you!")
+      )
+    }
+
 
     for (let fileName of fileNames) {
       let fileNameNoExt = fileName.split('.')[0]
 
-      if (previousFontName == fileNameNoExt || fileName == '.gitkeep') {
+      if (previousFontName == fileNameNoExt) {
         continue
       }
 
       let fontName = fileName.split('-')[0]
       let weight = parseNumericWeightFromName(fileName)
       let style = parseStyleFromName(fileName)
-      let isVariable = fileNameNoExt.toLocaleLowerCase().includes('variablefont') ? true : false
+      let isVariable = fileNameNoExt.toLocaleLowerCase().includes('variablefont')
 
 
       if (isVariable == false) {
-        writeFontFaceInFile(fontName, 'woff2', fileNameNoExt, weight, style)
+        await setupFontFaceRule(fileName, fontName, 'woff2', fileNameNoExt, weight, style)
       }
       else {
         weight = '100 1000'
-        writeFontFaceInFile(fontName, 'woff2-variations', fileNameNoExt, weight, style)
+        await setupFontFaceRule(fileName, fontName, 'woff2-variations', fileNameNoExt, weight, style)
       }
       previousFontName = fileNameNoExt
     }
   })
 }
-function writeFontFaceInFile(fontName, type, fileNameNoExt, weight, style) {
-  let fontFaceRule =
-    [
-      `@font-face {`,
-      `  font-style: ${style};`,
-      `  font-weight: ${weight};`,
-      `  src: url("../../fonts/${fileNameNoExt}.woff2") format("${type}");`,
-      `  font-family: "${fontName}";`,
-      `  font-display: swap;`,
-      `}`,
-    ]
+async function setupFontFaceRule(filename, fontName, type, fileNameNoExt, weight, style) {
+  let enquirerResult = await enquirer.snippet({
+    name: `${fontName}-snippet`,
+    message: `Set-up for ${filename}...`,
+    required: true,
+    fields: [
+      { name: 'style', initial: style },
+      { name: 'fontWeight', initial: weight.toString() },
+      { name: 'fontName', initial: fontName },
+    ],
+    template: `/* ${filename} */
+@font-face {
+  font-style: \${style};
+  font-weight: \${fontWeight};
+  src: url("../../fonts/${fileNameNoExt}.woff2") format("${type}");
+  font-family: "\${fontName}";
+  font-display: swap;
+}
+`,
 
-  fs.appendFileSync(fontsFIlePath, fontFaceRule.join('\r\n') + '\r\n')
+    footer: () => chalk.gray.italic("Use tab to move, when you're done, press enter")
+  })
+
+  fs.appendFileSync(fontsFIlePath, enquirerResult.result)
 }
