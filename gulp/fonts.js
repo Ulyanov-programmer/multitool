a60b84d5cbbebe10df88affaf3f47928ab962a01
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2023-08-24 10:58:11 +0400

    [fonts] Improved the font connection module.
    When writing fonts, create variables for easy connection (and stylelint rules).
    The check for identical file names has been removed (you can connect files with the same names, at your own risk).

--- gulp/fonts.js
+++ gulp/fonts.js
@@ -1,6 +1,6 @@
+import { $ } from '../gulpfile.js'
+
 import gulp from 'gulp'
-import ttf2woff2 from 'gulp-ttf2woff2'
-import changed from 'gulp-changed'
 import fs from 'fs-extra'
 import enquirer from 'enquirer'
 import chalk from 'chalk'
@@ -8,14 +8,14 @@ import { parseNumericWeightFromName, parseStyleFromName } from 'parse-font-name'
 import { fontsFilePath, paths } from './paths.js'
 
 export default function fonts() {
-  // woff2 files will just be copied.
+  // ? woff2 files will just be copied.
   gulp.src(paths.src.fontsWoff)
     .pipe(gulp.dest(paths.build.fonts))
 
   return gulp.src(paths.src.fonts)
-    .pipe(changed(paths.build.fonts, { extension: '.woff2' }))
+    .pipe($.changed(paths.build.fonts, { extension: '.woff2' }))
 
-    .pipe(ttf2woff2({
+    .pipe($.ttf2woff2({
       ignoreExt: true,
     }))
 
@@ -28,53 +28,52 @@ export function fontsStyle() {
   if (fontsFileContent.length > 0)
     return
 
+  let userFontNames = []
+
   fs.readdir(paths.src.fontsFolder, async (err, fileNames) => {
-    if (fileNames == undefined) {
+    if (filesIsCorrect(fileNames) == false) {
       console.log('No one font was found!')
       return
     }
 
-    let previousFontName
-    // removing the .gitkeep from fileNames.
-    if (fileNames.includes('.gitkeep')) {
-      fileNames.splice(fileNames.indexOf('.gitkeep'), 1)
-    }
-
-    if (fileNames.length == 0) {
-      console.log('No one font was found!')
-      return
-    } else {
-      console.log(
-        chalk.green.bold(`Hey, i see that you have fonts, but i haven't connected them yet. Let me help you!`)
-      )
-    }
+    console.log(
+      chalk.green.bold(`Hey, i see that you have fonts, but i haven't connected them yet. Let me help you!`)
+    )
 
 
     for (let fileName of fileNames) {
       let fileNameNoExt = fileName.split('.')[0]
 
-      if (previousFontName == fileNameNoExt) {
-        continue
-      }
-
       let fontName = fileName.split('-')[0]
       let weight = parseNumericWeightFromName(fileName)
       let style = parseStyleFromName(fileName)
       let isVariable = fileNameNoExt.toLocaleLowerCase().includes('variablefont')
+      let type
 
-
-      if (isVariable == false) {
-        await setupFontFaceRule(fileName, fontName, 'woff2', fileNameNoExt, weight, style)
-      }
-      else {
+      if (isVariable) {
+        type = 'woff2-variations'
         weight = '100 1000'
-        await setupFontFaceRule(fileName, fontName, 'woff2-variations', fileNameNoExt, weight, style)
+      } else {
+        type = 'woff2'
       }
-      previousFontName = fileNameNoExt
+
+      let userFontName = await setupFontFaceRule({
+        filename: fileName,
+        fontName: fontName,
+        type: type,
+        fileNameNoExt: fileNameNoExt,
+        weight: weight,
+        style: style,
+      })
+
+      userFontNames.push(userFontName)
     }
+
+    declareFontVariables(userFontNames)
   })
 }
-async function setupFontFaceRule(filename, fontName, type, fileNameNoExt, weight, style) {
+
+async function setupFontFaceRule({ filename, fontName, type, fileNameNoExt, weight, style }) {
   let enquirerResult = await enquirer.snippet({
     name: `${fontName}-snippet`,
     message: `Set-up for ${filename}...`,
@@ -98,4 +97,49 @@ async function setupFontFaceRule(filename, fontName, type, fileNameNoExt, weight
   })
 
   fs.appendFileSync(fontsFilePath, enquirerResult.result)
+
+  return enquirerResult.values.fontName
+}
+
+function declareFontVariables(fontNames) {
+  //? Removing duplicates
+  fontNames = [...new Set(fontNames)]
+
+  for (let i = 0; i < fontNames.length; i++) {
+    // ? Transform from FontName to css-property (--font-name: 'FontName';)
+    fontNames[i] = `--${toKebabCase(fontNames[i])}: '${fontNames[i]}';\n\t`
+
+    if (i == fontNames.length - 1) {
+      fontNames[i] = fontNames[i].replace('\n\t', '')
+    }
+  }
+
+  let variablesInRoot = `:root {
+  ${fontNames.toString().replaceAll(',', '')}
+}`
+
+  fs.appendFileSync(fontsFilePath, variablesInRoot)
+}
+
+function toKebabCase(string) {
+  let kebab = string.replace(/[A-Z]+(?![a-z])|[A-Z]/g,
+    ($, ofs) =>
+      (ofs ? "-" : "") + $.toLowerCase()
+  )
+
+  return kebab
+}
+
+function filesIsCorrect(fileNames) {
+  // ? removing the .gitkeep from fileNames.
+  if (fileNames && fileNames.includes('.gitkeep')) {
+    fileNames.splice(fileNames.indexOf('.gitkeep'), 1)
+
+    if (fileNames.length == 0) {
+      return false
+    }
+  }
+  else {
+    return false
+  }
 }
