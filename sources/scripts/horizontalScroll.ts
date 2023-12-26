b02f13f47a1ec27507477bc1a531397e88131d49
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2023-12-26 12:56:36 +0400

    [minor] Changes and fixes.

--- sources/scripts/horizontalScroll.ts
+++ sources/scripts/horizontalScroll.ts
@@ -1,16 +1,15 @@
-let elementsWithHorizontalScroll = document.querySelectorAll('.horizontal-scroll')
-
-function scrollHorizontally(e) {
-  let delta = Math.max(-1, Math.min(1, (-e.deltaY || -e.detail)))
-
-  e.currentTarget.scrollLeft -= (delta * 15)
-
-  e.preventDefault()
-}
-
-for (let element of elementsWithHorizontalScroll) {
+for (let element of document.querySelectorAll('.horizontal-scroll')) {
   // Chrome, Safari, Opera
   element.addEventListener('mousewheel', scrollHorizontally)
   // Firefox
   element.addEventListener('DOMMouseScroll', scrollHorizontally)
 }
+
+function scrollHorizontally(event: any) {
+  event.preventDefault()
+
+  let delta = Math.max(-1, Math.min(1, (-event.deltaY || -event.detail)))
+
+  event.currentTarget.scrollLeft -= (delta * 15)
+}
+
