1f1dd6ca183690b667165ec1e779ef5727297a9a
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2024-01-08 13:04:12 +0400

    [tabs] Redesigned using custom html elements.
    All functionality has been saved, new ones may have been added.
    
    Please read the added README.
    
    Added a demonstration of the functionality, run the project to try it.

---
+++ sources/scripts/tab/README.md
@@ -0,0 +1,48 @@
+# Instruction.
+
+For tabs to work, you need to connect scripts and styles: <br>
+(If you use gulp*multitool, you can connect them by switching `tabs=false` to `tabs=true` on the \_sources/index.html* page)
+
+```html
+<script src="./path_to_tab_folder/index.js"></script>
+<link rel="stylesheet" href="./path_to_tab_folder/index.css" />
+```
+
+After connecting, write the following HTML code:
+
+```html
+<tab-button aria-controls="tab-item1">The first item</tab-button>
+<tab-button aria-controls="tab-item2">The second item</tab-button>
+
+<tab-frame>
+  <tab-item id="tab-item1">content 1</tab-item>
+  <tab-item id="tab-item2">content 2</tab-item>
+</tab-frame>
+```
+
+Don't be embarrassed by the look of custom html elements, their support: https://caniuse.com/custom-elementsv1 (the author did not use the modification of embedded elements, so watch with partial support))
+
+- The _role_ attributes, _keyboard navigation_, and _aria-_ attributes are set automatically.
+- `tab-button` reveal the element whose identifier is specified in `aria-controls` (the element must be a `tab-item`).
+- The `expanded` attribute is assigned to the currently active buttons. The `current` attribute is assigned to the currently active tab-item.
+- By default, the _first_ element and _its buttons_ are current and expanded.
+
+You can use the following attributes for more fine-tuning:
+
+```html
+<!-- Enables fade mode. -->
+<tab-frame fade></tab-frame>
+
+<!-- Change the height of the tab-frame when changing the current tab-item. -->
+<tab-frame auto-height></tab-frame>
+
+<!-- 
+  Set the current attribute for one of the tab-item so that it and its buttons are active during initialization. 
+
+  If this attribute is not set for any tab-item, the first one in the parent tab-frame will be active.
+-->
+<tab-item id="some-id" current></tab-item>
+
+<!-- Specify the hover attribute so that the button can be clicked using mouse hover (the mouseenter event). -->
+<tab-button aria-controls="some-id" hover></tab-button>
+```
--- sources/scripts/tab/index.ts
+++ sources/scripts/tab/index.ts
@@ -1,21 +1,218 @@
-import Tab, { ToggleTabsEvent } from './tab.src.js'
+class HTMLTabButton extends HTMLElement {
+  public controlsTab: HTMLTabItem
 
-new Tab({
-  buttonsContainerSelector: '.tabButtons',
-  tabsContainerSelector: '.tabItems',
+  private _expanded: boolean
+  public get expanded(): boolean {
+    return this._expanded
+  }
+  public set expanded(v: boolean) {
+    v ? this.setAttribute('expanded', '') : this.removeAttribute('expanded')
+    this._expanded = v
+    this.ariaExpanded = v.toString()
+  }
 
-  fadeEffect: true,
-  autoHeight: true,
-  animationDuration: 500,
-  toggleTabsBy: ToggleTabsEvent.Click,
+  constructor() {
+    super()
+  }
 
-  /* 
-    ? Instead of using buttonActiveClass for styling, use the attribute aria-expanded, 
-    for example: button[aria-expanded="true"]
-  */
+  connectedCallback() {
+    this.role = 'tab'
+    this.tabIndex = 0
+    this.ariaExpanded = 'false'
+    this.expanded = Boolean(this.ariaExpanded)
 
-  /* 
-   ? Instead of using tabActiveClass for styling, use the attribute aria-current, 
-   for example: div[aria-current="true"]
-  */
-})
+    if (this.hasAttribute('hover')) {
+      this.addEventListener('mouseenter', this.openTab)
+    }
+    else {
+      this.addEventListener('click', this.openTab)
+    }
+
+    this.addEventListener('keyup', event =>
+      event.key == 'Enter' ? this.openTab() : false
+    )
+
+    document.addEventListener('DOMContentLoaded', this.init.bind(this))
+  }
+  init() {
+    this.controlsTab = document.getElementById(
+      this.getAttribute('aria-controls')
+    ) as HTMLTabItem
+  }
+
+  openTab() {
+    if (!this.controlsTab || this.controlsTab.current) return
+
+    this.controlsTab.parentFrame.toggleTabs(this.controlsTab)
+  }
+}
+class HTMLTabFrame extends HTMLElement {
+  private items: HTMLCollectionOf<HTMLTabItem>
+  private autoHeight: boolean
+  private containerHeight: number = 0
+  private isSwitching: boolean
+
+  private _isFade: boolean
+  public get isFade(): boolean {
+    return this._isFade
+  }
+  public set isFade(v: boolean) {
+    v ? this.setAttribute('fade', '') : this.removeAttribute('fade')
+    this._isFade = v
+  }
+
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.autoHeight = this.hasAttribute('auto-height')
+    this.isFade = this.hasAttribute('fade')
+
+    document.addEventListener('DOMContentLoaded', this.init.bind(this))
+    window.addEventListener('resize', this.setFrameHeight.bind(this))
+  }
+  init() {
+    this.items = this.children as HTMLCollectionOf<HTMLTabItem>
+
+    if (!this.autoHeight) {
+      // Setting the height of the largest child.
+      this.containerHeight = Math.max(...Array.from(this.items).map(item => item.clientHeight))
+    }
+    else {
+      this.containerHeight =
+        this.getCurrentItem()?.clientHeight ?? this.items[0].clientHeight
+    }
+
+    this.setFrameHeight(this.containerHeight)
+
+    this.setTabs()
+  }
+
+  private setTabs() {
+    if (!this.getCurrentItem()) {
+      this.activateFirstItem()
+      this.disableAnotherItems(this.items[0], true)
+    }
+  }
+
+  async toggleTabs(switchedItem: HTMLTabItem) {
+    if (switchedItem.current || this.isSwitching) return
+
+    this.isSwitching = true
+
+    this.disableAnotherItems(switchedItem)
+
+    if (!this.isFade) await sleep(switchedItem.transitionDurationValue)
+    if (this.autoHeight) this.setFrameHeight(switchedItem.clientHeight)
+
+    switchedItem.current = true
+
+    await sleep(switchedItem.transitionDurationValue)
+    this.isSwitching = false
+  }
+
+  private setFrameHeight(height: number | Event) {
+    if (typeof height != 'number') {
+      // Get the height of the current tab.
+      height = Array.from(this.items).find(item => item.current).clientHeight
+    }
+
+    this.style.height = height + 'px'
+  }
+  private disableAnotherItems(switchedItem: HTMLTabItem, isInitial?: Boolean) {
+    for (let item of this.items) {
+      if (item != switchedItem) {
+        if (isInitial) {
+          item.style.transition = 'none'
+          item.current = false
+
+          setTimeout(() => {
+            item.style.transition = ''
+          }, 30)
+        }
+        else {
+          item.current = false
+        }
+      }
+    }
+  }
+  private activateFirstItem() {
+    this.items[0].style.transition = 'none'
+    this.items[0].current = true
+
+    setTimeout(() => {
+      this.items[0].style.transition = ''
+    }, 50)
+  }
+  private getCurrentItem(): HTMLTabItem {
+    for (let item of this.items) {
+      var currentItem = item.current ? item : null
+    }
+
+    return currentItem
+  }
+}
+class HTMLTabItem extends HTMLElement {
+  public controlsButtons: NodeListOf<HTMLTabButton>
+  public parentFrame: HTMLTabFrame
+
+  private _transitionDurationValue: number
+  public get transitionDurationValue(): number {
+    return parseFloat(getComputedStyle(this)['transitionDuration']) * 1000
+  }
+
+  private _current: boolean
+  public get current(): boolean {
+    return this._current
+  }
+  public set current(v: boolean) {
+    this._current = v
+
+    this.controlsButtons = this.controlsButtons ??
+      document.querySelectorAll(`tab-button[aria-controls=${this.id}]`)
+
+    v ? this.show() : this.hide()
+  }
+
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.role = 'tabpanel'
+    this.parentFrame = this.parentElement as HTMLTabFrame
+    this.current = this.hasAttribute('current')
+
+    document.addEventListener('DOMContentLoaded', this.init.bind(this))
+  }
+  init() {
+    this.controlsButtons = this.controlsButtons ??
+      document.querySelectorAll(`tab-button[aria-controls=${this.id}]`)
+  }
+
+  private hide() {
+    this.removeAttribute('current')
+    this.ariaCurrent = 'false'
+
+    for (let button of this.controlsButtons) {
+      button.expanded = false
+    }
+  }
+  private show() {
+    this.setAttribute('current', '')
+    this.ariaCurrent = 'true'
+
+    for (let button of this.controlsButtons) {
+      button.expanded = true
+    }
+  }
+}
+
+customElements.define('tab-button', HTMLTabButton)
+customElements.define('tab-frame', HTMLTabFrame)
+customElements.define('tab-item', HTMLTabItem)
+
+function sleep(timeMs: number): Promise<any> {
+  return new Promise(r => setTimeout(r, timeMs))
+}
