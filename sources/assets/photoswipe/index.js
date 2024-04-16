99337786fdee406d69e6235507f855a0b9bf55e4
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2024-04-16 09:56:56 +0400

    [assets] Added code to the PhotoSwipe initializer.

--- sources/assets/photoswipe/index.js
+++ sources/assets/photoswipe/index.js
@@ -1,8 +1,28 @@
+/*
+  https://photoswipe.com/getting-started/
+*/
+
 import PhotoSwipeLightbox from './photoswipe-lightbox.esm.min.js'
-import PhotoSwipe from './photoswipe.esm.min.js'
 
 new PhotoSwipeLightbox({
   gallery: '#your_gallery',
   children: 'a',
-  pswpModule: PhotoSwipe,
+  pswpModule: () => import('./photoswipe.esm.min.js'),
 }).init()
+
+
+/* HINTS
+
+  arrowPrev: false,
+  arrowNext: false,
+  zoom: false,
+  close: false,
+  counter: false,
+
+  ? set background opacity
+  bgOpacity: 0.6,
+
+  ? to apply styles just to an instance of PhotoSwipe
+  mainClass: 'YourClass',
+
+*/
