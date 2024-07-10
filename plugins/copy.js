cd284e6a41dd996cf20b2e717628c122fe2cf573
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2024-07-10 12:23:17 +0400

    [copy] Added a check to protect against unnecessary triggering.

--- plugins/copy.js
+++ plugins/copy.js
@@ -2,6 +2,12 @@ import fs from 'fs-extra'
 import chalk from 'chalk'
 
 export default function () {
+  let assetsFolderCtime = fs.statSync(globalThis.paths.sources.assets)?.ctimeMs
+  let distAssetsFolderCtime = fs.statSync(globalThis.paths.output.assets)?.ctimeMs
+
+  if (assetsFolderCtime <= distAssetsFolderCtime)
+    return
+
   fs.copySync(globalThis.paths.sources.assets, globalThis.paths.output.assets)
 
   console.log(
