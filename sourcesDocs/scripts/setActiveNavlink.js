b226fbe608740fbb740f42dc996f4d81dd3993d4
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2024-11-18 12:21:51 +0400

    [general] Deleting unfinished items.

--- sourcesDocs/scripts/setActiveNavlink.js
+++
@@ -1,9 +0,0 @@
-const pageTitle = document.title
-const links = Array.from(document.querySelectorAll('#main-navigation a'))
-
-let linkThatShouldBeActive = links.find(link => {
-  let refText = link.textContent.trim().replaceAll('\n', '')
-  return refText == pageTitle
-})
-
-linkThatShouldBeActive.classList.add('active')
